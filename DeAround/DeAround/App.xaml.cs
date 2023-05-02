b0aff04ffb3c04d52971821785913586b8835950
Israel Soto <israel.spgh@gmail.com>
2023-05-02 11:11:06 -0600

    [Forms] Worked on UI * Added a CollectionView where data will be displayed * Added a RefreshView * Added a button to start searching for devices * Added support for C# 9 everywhere

--- DeAround/DeAround/App.xaml.cs
+++ DeAround/DeAround/App.xaml.cs
@@ -2,28 +2,26 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace DeAround
-{
-    public partial class App : Application
-    {
-        public App ()
-        {
-            InitializeComponent();
+namespace DeAround {
+	public partial class App : Application {
+		public App ()
+		{
+			InitializeComponent ();
 
-            MainPage = new MainPage();
-        }
+			MainPage = new NavigationPage (new MainPage ());
+		}
 
-        protected override void OnStart ()
-        {
-        }
+		protected override void OnStart ()
+		{
+		}
 
-        protected override void OnSleep ()
-        {
-        }
+		protected override void OnSleep ()
+		{
+		}
 
-        protected override void OnResume ()
-        {
-        }
-    }
+		protected override void OnResume ()
+		{
+		}
+	}
 }
 

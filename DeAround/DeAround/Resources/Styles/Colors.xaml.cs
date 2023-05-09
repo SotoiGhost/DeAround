9c83663b1aec2aa0c94dca6faea0ac92bb796d61
Israel Soto <israel.spgh@gmail.com>
2023-05-09 01:34:30 -0600

    [Forms] Added light and dark themes

---
+++ DeAround/DeAround/Resources/Styles/Colors.xaml.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DeAround.Resources.Styles {
+	public partial class Colors : ResourceDictionary {
+		public Colors ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ DeAround/DeAround/Resources/Styles/Styles.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace DeAround.Resources.Styles {
+	public partial class Styles : ResourceDictionary {
+		public Styles ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
